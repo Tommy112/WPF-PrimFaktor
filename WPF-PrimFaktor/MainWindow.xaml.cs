2f1cd9c5b752febc6dcfc294caa60c823d0889c0
Tomanderson <tom612k@gmail.com>
2021-10-25 12:00:26 +0200

    Add project files.

---
+++ WPF-PrimFaktor/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace WPF_PrimFaktor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int zahl;
+        string eingabe, ergebnis;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PrimFaktor primFaktor = new PrimFaktor();
+
+            eingabe = Input.Text;
+
+            bool pruefung = int.TryParse(eingabe, out zahl);
+
+            if (pruefung && zahl > 1)
+            {
+                Input.Text = "";
+                ergebnis = primFaktor.Zerlegung(zahl);
+            }
+            else
+            {
+                Input.Text = "";
+                MessageBox.Show("Bitte eine Zahl größer 2 eingeben");
+            }
+
+            Output.Text = ergebnis;
+        }
+    }
+}
---
+++ WPF-PrimFaktor/PrimFaktor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPF_PrimFaktor
+{
+    class PrimFaktor
+    {
+        int prufZahl;
+        int teiler = 3;
+        public List<String> primFaktoren = new List<String>();
+
+        public string Zerlegung(int eingabe)
+        {
+            prufZahl = eingabe;
+
+            while (eingabe % 2 == 0)
+            {
+                primFaktoren.Add("2");
+                eingabe /= 2;
+            }
+
+            while (eingabe > 1)
+            {
+                while (eingabe % teiler == 0)
+                {
+                    primFaktoren.Add(teiler.ToString());
+                    eingabe /= teiler;
+                }
+                teiler += 2;
+            }
+
+            if (primFaktoren.Count == 1)
+            {
+                return prufZahl.ToString() + " ist eine Primzahl";
+            }
+            else
+            {
+                string output = string.Join("-", primFaktoren);
+                return "Die Zerlegung ist " + output;
+            }
+
+            
+        }
+    }
+}
